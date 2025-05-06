97b93e8fb7d74a73148d05fca9a697b8b65a78a4
eNgMu77 <engmu7a3d@gmail.com>
2025-05-06 21:31:05 +0300

    init hive new

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/constant.dart';
 import 'package:note_app/views/notes_view.dart';
+// import 'package:path_provider/path_provider.dart';
+
 
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
   runApp(const NoteApp());
